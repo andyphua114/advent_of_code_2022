680f3bf97aea3aea787ec1d31a0cb1192d61f178
Andy Phua <andyphuawc@gmail.com>
2022-12-10 08:58:15 +0800

    Add files via upload

---
+++ day_01/aoc_day1.py
@@ -0,0 +1,26 @@
+with open('aoc_input1.1.txt') as f:
+    contents = f.readlines()
+
+food = []
+total_cal = 0
+idx = 0
+for i in contents:
+    cal = i.replace('\n', '')
+    if cal != '':
+        total_cal = total_cal + int(cal)
+    else:
+        food.append(total_cal)
+        total_cal = 0
+    if idx == len(contents)-1:
+        food.append(total_cal)
+    idx += 1
+
+print(max(food))
+
+food.sort(reverse=True)
+
+top3_food = 0
+for i in range(3):
+    top3_food += food[i]
+
+print(top3_food)
