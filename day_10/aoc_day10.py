dedb47afd772c011115e4cc39ae8b705a9a10d41
andyphua114 <69377759+andyphua114@users.noreply.github.com>
2022-12-10 13:50:22 +0800

    working part1

--- day_10/aoc_day10.py
+++ day_10/aoc_day10.py
@@ -1,17 +1,16 @@
-with open("sample.txt") as file:
+with open("input.txt") as file:
     lines = [line.rstrip() for line in file]
 
 # print(lines)
 
-instructions = []
 add = []
 
 for l in lines:
     if l[0] == 'a':
+        add.append(0)
         add.append(int(l[5:]))
     else:
         add.append(0)
-    instructions.append(l[0])
 
 # print(add)
 
@@ -19,25 +18,18 @@ cycle = 0
 signal = 1
 signal_history = []
 
-for i in instructions:
-    if i == 'n':
-        signal_history.append(signal)
-        cycle += 1
-    else:
-        signal_history.append(signal)
-        signal += add[cycle]
-        signal_history.append(signal)
-        cycle += 1
+for a in add:
+    signal += a
+    signal_history.append(signal)
 
 index = [20, 60, 100, 140, 180, 220]
 signal_sum = 0
 
 for i in index:
-    signal_sum += i * signal_history[i-1]
+    signal_sum += i * signal_history[i-2]
     # print(signal_history[i-1])
 
-# print(signal_sum)
-print(signal_history)
+print(signal_sum)
 
 
 
